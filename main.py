59c9fe6bbe4a1acbf146092a00d0879e31107a50
tolyamba75 <tolyamironov75@yandex.ru>
2020-07-25 01:50:29 +0300

    Add main.py

--- main.py
+++ main.py
@@ -3,69 +3,44 @@ from vkprocess import VkProcess
 from wordgame import WordGame
 
 
-def json_data(data):
-    post_id = data['object']['post_id']
-    user_id = data['object']['from_id']
-    comment_num = data['object']['id']
-    text = data['object']['text']
-    return post_id, user_id, comment_num, text
+class MiniGame:
+    def __init__(self, data, group_id, token):
+        self.data = data
+        self.group_id = group_id
+        self.token = token
+
+        self.post_id = self.data['object']['post_id']
+        self.user_id = self.data['object']['from_id']
+        self.num_comment = self.data['object']['id']
+        self.text = self.data['object']['text']
+
+        self.vk_tool = VkProcess(group_id, token)
+        self.name = self.vk_tool.check_name(self.user_id)
+
+        try:
+            self.comment = game[self.post_id].answer(self.text)
+        except KeyError as e:
+            print('invalid key', e)
+
+    def print_comment(self):
+        message = '[id{user_id}|{name}], {comment}.'.format(user_id=self.user_id, name=self.name,
+                                                            comment=self.comment)
+        if self.vk_tool.check_subscribe(self.user_id):
+            message += ' Пожалуйста, подпишитесь'
+        self.vk_tool.reply(message, self.num_comment, self.post_id)
+
+    def stop_game(self, post_num):
+        try:
+            game[post_num].game_over()
+            self.vk_tool.off_comments(post_num)
+        except KeyError as e:
+            print('no key', e)
 
 
-def answer_comment(ans):
-    if ans and dct[0] == first_post:
-        comment = 'ранил'
-    elif ans and dct[0] == second_post:
-        comment = 'правильно'
-
-    elif ans is None and dct[0] == first_post:
-        comment = 'мимо'
-    elif ans is None and dct[0] == second_post:
-        comment = 'слово засчитано + 1 балл'
-
-    else:
-        comment = 'попытайся еще раз'
-    return comment
-
-
-def print_comment():
-    if dct[0] == first_post:
-        ans = battle.answer(dct[3])
-        com = answer_comment(ans)
-
-    elif dct[0] == second_post:
-        ans = word.answer((dct[3]))
-        com = answer_comment(ans)
-
-    else:
-        com = ''
-
-    user_id = dct[1]
-    name = vk_tool.check_name(user_id)
-    message = '[id{user_id}|{name}], {comment}.'.format(user_id=user_id, name=name, comment=com)
-    if vk_tool.check_subscribe(user_id):
-        message += ' Пожалуйста, подпишитесь'
-    vk_tool.reply(message, dct[2], dct[0])
-
-
-def stop_game(post):
-    if post == first_post:
-        battle.game_over()
-    elif post == second_post:
-        word.game_over()
-
-
-group_id = 51312
-token = ''
-dct = {}
-dct = json_data(dct)
-vk_tool = VkProcess(group_id, token)
-
-# SeaBATTLE
-first_post = 1
+# group_id = 51312
+# token = ''
+# data = {}
 win_lst = ['в2', 'г3', 'д5', 'е5', 'б6']
-battle = SeaBattle(win_lst)
+letter = 'ф'
+game = {1: SeaBattle(win_lst), 6: WordGame(letter)}
 
-# WordGAME
-second_post = 6
-litter = 'ф'
-word = WordGame(litter)
