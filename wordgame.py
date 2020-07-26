411b812833564cfc212d1abd2214fb4dcbcf5a42
tolyamba75 <tolyamironov75@yandex.ru>
2020-07-26 15:43:22 +0300

    Add wordgame.py

--- wordgame.py
+++ wordgame.py
@@ -3,17 +3,24 @@ class WordGame:
         self.ans_opt = ['правильно.', 'слово уже было отгадано.', 'попытайся еще раз.']
         self.letter = letter.lower()
         self.lst = []
+        # id: кол-во отгаданных
+        self.lst_part = {}
 
     def rules(self, word):
         if self.letter == word[:1]:
             return True
         return False
 
-    def answer(self, word):
+    def answer(self, word, user_id):
         word = word.lower()
         # угадал
         if self.rules(word) and not(word in self.lst):
             self.lst.append(word)
+            try:
+                self.lst_part[user_id] += 1
+            except KeyError:
+                self.lst_part[user_id] = 1
+
             return self.ans_opt[0]
         # слово отгадано
         elif self.rules(word):
@@ -25,4 +32,7 @@ class WordGame:
     def game_over(self):
         self.letter = ' '
         return self.lst
+
+    def win(self):
+        return self.lst_part
 
