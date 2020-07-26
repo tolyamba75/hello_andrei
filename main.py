70c88f75b4d465e3aa70359e6db564ae4bc0b906
tolyamba75 <tolyamironov75@yandex.ru>
2020-07-26 15:39:19 +0300

    Add main.py

--- main.py
+++ main.py
@@ -17,24 +17,34 @@ class MiniGame:
         self.vk_tool = VkProcess(group_id, token)
         self.name = self.vk_tool.check_name(self.user_id)
 
+    def print_comment(self):
         try:
-            self.comment = game[self.post_id].answer(self.text)
+            if self.vk_tool.check_subscribe(self.user_id):
+                comment = game[self.post_id].answer(self.text, self.user_id)
+                message = '[id{user_id}|{name}], {comment}.'.format(user_id=self.user_id, name=self.name,
+                                                                    comment=comment)
+            else:
+                message = '[id{user_id}|{name}], ответ не засчитан. Подпишитесь'.format(user_id=self.user_id,
+                                                                                        name=self.name)
+            self.vk_tool.reply(message, self.num_comment, self.post_id)
+
         except KeyError as e:
             print('invalid key', e)
-
-    def print_comment(self):
-        message = '[id{user_id}|{name}], {comment}.'.format(user_id=self.user_id, name=self.name,
-                                                            comment=self.comment)
-        if self.vk_tool.check_subscribe(self.user_id):
-            message += ' Пожалуйста, подпишитесь'
-        self.vk_tool.reply(message, self.num_comment, self.post_id)
 
     def stop_game(self, post_num):
         try:
             game[post_num].game_over()
-            self.vk_tool.off_comments(post_num)
+            self.vk_tool.off_comments(post_num, self.winners(post_num))
         except KeyError as e:
             print('no key', e)
+
+    def winners(self, post_num):
+        lst = list(game[post_num].win().items())
+        lst.sort(key=lambda i: i[1])
+        user_name = self.vk_tool.check_name(lst[-1][0])
+        messages = 'Первое место: [id{user_id}|{name}],' \
+                   ' количество правильных ответов: {num}'.format(user_id=lst[-1][0], name=user_name, num=[-1][1])
+        return messages
 
 
 # group_id = 51312
