05f4d996607fc002bf2dad6a0c1b0f632503581a
tolyamba75 <tolyamironov75@yandex.ru>
2020-07-26 15:42:17 +0300

    Add vkprocess.py

--- vkprocess.py
+++ vkprocess.py
@@ -2,29 +2,31 @@ import vk_api
 
 
 class VkProcess:
-    def __init__(self, token, group_id):
+    def __init__(self, group_id, token):
         vk_session = vk_api.VkApi(token=token)
         self.vk = vk_session.get_api()
         self.group_id = group_id    # положительное число
 
     # ответ на комментарий
-    def reply(self, message, post_id, num_comment):
+    def reply(self, message, num_comment, post_id):
         self.vk.wall.createComment(owner_id=-self.group_id, post_id=post_id,
                                    from_group=self.group_id, message=message,
                                    reply_to_comment=num_comment)
 
     # выключение комментариев
-    def off_comments(self, post_id):
+    def off_comments(self, post_id, message):
+        self.vk.wall.createComment(owner_id=-self.group_id, post_id=post_id,
+                                   from_group=self.group_id, message=message)
         self.vk.wall.closeComments(owner_id=-self.group_id, post_id=post_id)
 
-    # проверка лайка
+    # проверка подписки
     def check_subscribe(self, user_id):
         subscribe = self.vk.groups.isMember(group_id=self.group_id, user_id=user_id)
         if subscribe:
             return True
         else:
             return False
-    
+
     def check_name(self, users_id):
         name = self.vk.users.get(user_ids=users_id)[0]['first_name']
         return name
